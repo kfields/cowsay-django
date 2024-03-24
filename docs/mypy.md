31ce15a63054584b0dee7edf38e326ce4e278722
kfields <kurtisfields@gmail.com>
2024-03-24 07:09:17 -0600

    make mypy happy

---
+++ docs/mypy.md
@@ -0,0 +1,7 @@
+```bash
+hatch -e types shell
+```
+
+```bash
+stubgen -p cowsay -o ./stubs
+```
---
+++ stubs/cowsay/__init__.pyi
@@ -0,0 +1,6 @@
+from .main import CowsayError as CowsayError, __version__ as __version__, char_names as char_names, get_output_string as get_output_string
+from _typeshed import Incomplete
+
+char_funcs: Incomplete
+
+def func(text: str, char_lines=...): ...
